e4f4023122784be70bbcd1ca8727e2d4c85fdb0c
Jerin <jerinrx@gmail.com>
2025-09-09 12:31:09 +0530

    Initial commit: Added project files

--- lib/src/widgets/html_editor_widget_web.dart
+++ lib/src/widgets/html_editor_widget_web.dart
@@ -78,30 +78,43 @@ class _HtmlEditorWidgetWebState extends State<HtmlEditorWidget> {
   void initSummernote() async {
     var headString = '';
     var summernoteCallbacks = '''callbacks: {
-        onKeydown: function(e) {
-            var chars = \$(".note-editable").text();
-            var totalChars = chars.length;
-            ${widget.htmlEditorOptions.characterLimit != null ? '''allowedKeys = (
-                e.which === 8 ||  /* BACKSPACE */
-                e.which === 35 || /* END */
-                e.which === 36 || /* HOME */
-                e.which === 37 || /* LEFT */
-                e.which === 38 || /* UP */
-                e.which === 39 || /* RIGHT*/
-                e.which === 40 || /* DOWN */
-                e.which === 46 || /* DEL*/
-                e.ctrlKey === true && e.which === 65 || /* CTRL + A */
-                e.ctrlKey === true && e.which === 88 || /* CTRL + X */
-                e.ctrlKey === true && e.which === 67 || /* CTRL + C */
-                e.ctrlKey === true && e.which === 86 || /* CTRL + V */
-                e.ctrlKey === true && e.which === 90    /* CTRL + Z */
-            );
-            if (!allowedKeys && \$(e.target).text().length >= ${widget.htmlEditorOptions.characterLimit}) {
-                e.preventDefault();
-            }''' : ''}
-            window.parent.postMessage(JSON.stringify({"view": "$createdViewId", "type": "toDart: characterCount", "totalChars": totalChars}), "*");
-        },
-    ''';
+  onInit: function() {
+    var editable = document.querySelector('.note-editable');
+    if (editable) {
+      editable.addEventListener('copy', function(e) { e.preventDefault(); });
+      editable.addEventListener('paste', function(e) { e.preventDefault(); });
+    }
+    document.addEventListener('copy', function(e) {
+      if (e.target.closest('.note-editable')) e.preventDefault();
+    });
+    document.addEventListener('paste', function(e) {
+      if (e.target.closest('.note-editable')) e.preventDefault();
+    });
+  },
+  onKeydown: function(e) {
+      var chars = \$(".note-editable").text();
+      var totalChars = chars.length;
+      ${widget.htmlEditorOptions.characterLimit != null ? '''allowedKeys = (
+          e.which === 8 ||  /* BACKSPACE */
+          e.which === 35 || /* END */
+          e.which === 36 || /* HOME */
+          e.which === 37 || /* LEFT */
+          e.which === 38 || /* UP */
+          e.which === 39 || /* RIGHT*/
+          e.which === 40 || /* DOWN */
+          e.which === 46 || /* DEL*/
+          e.ctrlKey === true && e.which === 65 || /* CTRL + A */
+          e.ctrlKey === true && e.which === 88 || /* CTRL + X */
+          e.ctrlKey === true && e.which === 67 || /* CTRL + C */
+          e.ctrlKey === true && e.which === 86 || /* CTRL + V */
+          e.ctrlKey === true && e.which === 90    /* CTRL + Z */
+      );
+      if (!allowedKeys && \$(e.target).text().length >= ${widget.htmlEditorOptions.characterLimit}) {
+          e.preventDefault();
+      }''' : ''}
+      window.parent.postMessage(JSON.stringify({"view": "$createdViewId", "type": "toDart: characterCount", "totalChars": totalChars}), "*");
+  },
+''';
     var maximumFileSize = 10485760;
     for (var p in widget.plugins) {
       headString = headString + p.getHeadString() + '\n';
@@ -200,23 +213,18 @@ class _HtmlEditorWidgetWebState extends State<HtmlEditorWidget> {
     if (widget.callbacks != null) {
       jsCallbacks = getJsCallbacks(widget.callbacks!);
     }
-   var userScripts = '''
-$disableCopyPasteJS
-''';
-
-if (widget.htmlEditorOptions.webInitialScripts != null) {
-  widget.htmlEditorOptions.webInitialScripts!.forEach((element) {
-    userScripts +=
-      '''
-      if (data["type"].includes("${element.name}")) {
-        ${element.script}
-      }
-      ''' +
-      '\n';
-  });
-}
-
-
+    var userScripts = '';
+    if (widget.htmlEditorOptions.webInitialScripts != null) {
+      widget.htmlEditorOptions.webInitialScripts!.forEach((element) {
+        userScripts = userScripts +
+            '''
+          if (data["type"].includes("${element.name}")) {
+            ${element.script}
+          }
+        ''' +
+            '\n';
+      });
+    }
     var summernoteScripts = """
       <script type="text/javascript">
         \$(document).ready(function () {
